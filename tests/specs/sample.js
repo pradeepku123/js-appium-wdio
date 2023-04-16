faf349f61dc8af9e95fac739bad091bf109d5421
pradeepku123 <pradeepkumarstudent2011@gmail.com>
2023-04-16 16:36:55 +0530

    Intraction with Multiple Screen

--- tests/specs/sample.js
+++ tests/specs/sample.js
@@ -1,5 +1,20 @@
-describe("DEC-011", () => {
-  it("IT", async () => {
-    console.log(`Hello world`);
+describe("Android Demo Application", () => {
+  xit("Interacting Android Element", async () => {
+    const findElementFirst = await $("android.widget.TextView");
+
+    await expect(findElementFirst).toHaveText("API Demos");
+  });
+
+  it("Intracting Multiple Screen", async () => {
+    const findElementFirst = await $("android.widget.TextView");
+    await expect(findElementFirst).toHaveText("API Demos");
+    await expect(await $("~App")).toHaveText("App");
+    await $("~App").click();
+    //Action Bar
+    await expect(await $("~Action Bar")).toHaveText("Action Bar");
+    await $("~Action Bar").click();
+    await expect(await $("~Action Bar Mechanics")).toHaveText(
+      "Action Bar Mechanics"
+    );
   });
 });
